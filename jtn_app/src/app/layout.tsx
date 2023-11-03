6406026b9bbbd4722dcfc37f0a0eeb848d448a60
Helge-Mikael Landro <landro@Landro-MacBook-Pro.local>
2023-11-03 13:30:01 +0100

    SEO stuff

--- jtn_app/src/app/layout.tsx
+++ jtn_app/src/app/layout.tsx
@@ -5,6 +5,27 @@ export const metadata: Metadata = {
   title: "Jahn Terje Nilsen & Gjengen",
   description:
     "Som gatevant i Bergens mangefasetterte musikkscene stikker Jahn Terje Nilsen & Gjengen frem et frekt og freidig musikalsk åsyn. Husvarm i både rock, rap, i bedehus og på nattklubben, serveres innblikk i menneskesjelen og utsikter fra Ulriken.",
+  authors: [
+    {
+      name: "Helge-Mikael Landro",
+      url: "https://www.instagram.com/herrlandro/",
+    },
+    {
+      name: "Jahn Terje Nilsen",
+      url: "https://www.instagram.com/jtnilsen83/",
+    },
+    {
+      name: "Jan-Christoph Cassel Noven",
+      url: "https://www.instagram.com/jahnterjenilsen_og_gjengen/",
+    },
+    {
+      name: "Jahn Terje Nilsen og Gjengen",
+      url: "https://www.instagram.com/jahnterjenilsen_og_gjengen/",
+    },
+  ],
+  keywords: [
+    "Jahn Terje Nilsen & Gjengen, Jahn Terje Nilsen og Gjengen, Helge-Mikael Landro, Jan-Christoph Cassel Noven, Jahn Terje Nilsen, Eg ser du har lest, Mine grunner privat, Sammenkomst",
+  ],
 };
 
 export default function RootLayout({
