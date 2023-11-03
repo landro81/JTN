64001cb020c7957fe017fd586211b06af17e2798
Helge-Mikael Landro <landro@Landro-MacBook-Pro.local>
2023-11-03 10:54:36 +0100

    Add color comp

---
+++ jtn_app/src/assets/color/index.ts
@@ -0,0 +1,5 @@
+export const Color = {
+  JTN_PURPLE: "#ff00ff",
+  JTN_YELLOW: "#ffff00",
+  JTN_GREY_TEXT: "#c3c3c3",
+};
--- jtn_app/src/components/Footer/index.tsx
+++ jtn_app/src/components/Footer/index.tsx
@@ -1,24 +1,29 @@
 import Link from "next/link";
 import Typography from "../Typography";
 import { TextResources } from "@/assets/text";
+import { Color } from "@/assets/color";
 
-export const Footer = () => (
-  <div className="box-border w-full flex flex-col items-center justify-center h-[120px] bg-white lg:h-[140px] tracking-tight mt-auto overflow-x-hidden">
-    <Typography color="#c3c3c3" variant="Bold">
-      {TextResources.BAND_NAME}
-    </Typography>
-    <Typography color="#c3c3c3" variant="Bold">
-      {TextResources.COPYRIGHT}
-    </Typography>
-    <Link target="_blank" href={"https://www.instagram.com/jarlehm/"}>
-      <Typography variant="Description" color="#c3c3c3">
-        {TextResources.PHOTOGRAPHER_NAME}
+export const Footer = () => {
+  const color = Color.JTN_GREY_TEXT;
+
+  return (
+    <div className="box-border w-full flex flex-col items-center justify-center h-[120px] bg-white lg:h-[140px] tracking-tight mt-auto overflow-x-hidden">
+      <Typography color={color} variant="Bold">
+        {TextResources.BAND_NAME}
       </Typography>
-    </Link>
-    <Link target="_blank" href={"https://www.instagram.com/herrlandro/"}>
-      <Typography variant="Description" color="#c3c3c3">
-        {TextResources.DEV_NAME}
+      <Typography color={color} variant="Bold">
+        {TextResources.COPYRIGHT}
       </Typography>
-    </Link>
-  </div>
-);
+      <Link target="_blank" href={"https://www.instagram.com/jarlehm/"}>
+        <Typography variant="Description" color={color}>
+          {TextResources.PHOTOGRAPHER_NAME}
+        </Typography>
+      </Link>
+      <Link target="_blank" href={"https://www.instagram.com/herrlandro/"}>
+        <Typography variant="Description" color={color}>
+          {TextResources.DEV_NAME}
+        </Typography>
+      </Link>
+    </div>
+  );
+};
