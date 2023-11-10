63407b1870de7a9fee45ed2ff6285954d9e09520
Helge-Mikael Landro <landro@Landro-MacBook-Pro.local>
2023-11-10 13:30:52 +0100

    Update spotify

--- jtn_app/src/components/SpotifyComponent/index.tsx
+++ jtn_app/src/components/SpotifyComponent/index.tsx
@@ -5,7 +5,7 @@ export const SpotifyComponent = () => {
     <div className="flex flex-col items-center justify-center mt-3 mb-7">
       <Spotify
         width={"350px"}
-        link="https://open.spotify.com/album/1lvGzyBSCeUl7J0Cs40Fn0?si=6y_V0IDKRHG9f1VkN65JSQ&nd=1"
+        link="https://open.spotify.com/track/7K3HrZJlOwZ5b8iI7SefKK?si=070cf91fa11f4477"
       />
     </div>
   );
