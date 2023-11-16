3984ed0e3198f13a84a17a64cb9f8f1a034b9e0d
Helge-Mikael Landro <landro@Landro-MacBook-Pro.local>
2023-11-16 09:01:16 +0100

    Logo adjustments

--- jtn_app/src/components/BandLogo/index.tsx
+++ jtn_app/src/components/BandLogo/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export const BandLogo = () => (
-  <div className="flex z-50 absolute right-[-6px] top-[-24px] justify-end w-[150px] sm:w-[200px] md:min-w-[300px] lg:min-w-[400px]">
+  <div className="flex z-50 absolute right-[-6px] top-[-25px] justify-end w-[150px] sm:w-[200px] md:min-w-[300px] lg:min-w-[400px] sm:top-[-34px] md:top-[-50px] lg:top-[-68px]">
     <Image
       src={"/graphics/JTN_logo.png"}
       alt="Logo Jahn Terje Nilsen og gjengen"
