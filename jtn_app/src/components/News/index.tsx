345f7701a16f60297bccdf7bd4ab1057081f0550
Helge-Mikael Landro <helge.mikael@ilder.no>
2024-02-16 11:09:12 +0100

    Sammenkomst tweaks

--- jtn_app/src/components/News/index.tsx
+++ jtn_app/src/components/News/index.tsx
@@ -6,7 +6,7 @@ import Typography from "../Typography";
 export const News = () => {
   return (
     <>
-      <div className="flex bg-[#ffff00] px-2 mt-[-15px] md:mt-[-20px] lg:mt-[-30px] xl:mt-[-35px] ml-[-90px] md:ml-[-230px] h-[auto] max-h-[50px] items-center">
+      <div className="flex bg-[#ffff00] px-2 mt-[-15px] md:mt-[-20px] lg:mt-[-30px] xl:mt-[-35px] ml-[-80px] md:ml-[-230px] h-[auto] max-h-[50px] items-center">
         <h1 className="flex text-[1.5em] md:text-[2em] lg:text-[3em] xl:text-[3em] tracking-tightest font-sans">
           {TextResources.NEWS_HEADER}
         </h1>
@@ -14,7 +14,7 @@ export const News = () => {
       <div className="flex flex-col bg-[#ffff00] px-2 py-1 mt-[4px] max-w-[330px] md:max-w-[430px] lg:max-w-[500px] md:ml-[-110px]">
         <Typography variant="Body">{TextResources.NEWS_CONTENT}</Typography>
       </div>
-      <div className="flex flex-col bg-[#ffff00] underline px-5 mt-[4px] ml-[-90px] md:ml-[-325px] lg:ml-[-425px] h-[auto] max-h-[50px] items-center">
+      <div className="flex flex-col bg-[#ffff00] underline px-5 mt-[9px] ml-[-90px] md:ml-[-325px] lg:ml-[-425px] h-[auto] max-h-[50px] items-center">
         <Link href={"https://orcd.co/1d460xl"} target="_blank" rel="noopener">
           <h1 className="flex text-[1.1em] tracking-tighter font-sans">
             {TextResources.NEWS_SONG}
--- jtn_app/src/components/SpotifyComponent/index.tsx
+++ jtn_app/src/components/SpotifyComponent/index.tsx
@@ -2,14 +2,14 @@ import { Spotify } from "react-spotify-embed";
 
 export const SpotifyComponent = () => (
   <>
-    <div className="flex flex-col items-center justify-center mt-7">
+    <div className="flex flex-col items-center justify-center mt-6">
       <Spotify
         wide
         width={"350px"}
         link="https://open.spotify.com/track/2ptdRghyA5p4uiQphX3WEN?si=7a49facba3c74903"
       />
     </div>
-    <div className="flex flex-col items-center justify-center mb-7 sm:mt-4 md:mt-4">
+    <div className="flex flex-col items-center justify-center mb-7 mt-2 md:mt-4">
       <Spotify
         wide
         width={"350px"}
